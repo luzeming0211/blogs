0d1bca23cfe14f61eb5616d68fcdc5b579c5104c
卢泽明 <luzeming@medlive.cn>
2020-06-19 14:33:53 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 Route::get('/', 'ArticleController@index');
 Route::get('/{id}', 'ArticleController@detail');
+
+
+
 Route::get('/user/{userid}', 'ArticleController@user');
 Route::get('/user/{userid}/{id}', 'ArticleController@');
