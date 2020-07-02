a83d74a900dbcbcc75c988c80318ffb949a381a6
卢泽明 <luzeming@medlive.cn>
2020-07-02 13:05:39 +0800

    222

--- routes/websocket.php
+++ routes/websocket.php
@@ -8,14 +8,18 @@ use SwooleTW\Http\Websocket\Facades\Websocket;
 $redis = '\Illuminate\Support\Facades\Redis';
 Websocket::on('message', function ($websocket, $data) use ($redis) {
     if (is_array($data)) {
-        $key_client = 'client:' . Websocket::getSender();
+//        $key_client = 'client:' . Websocket::getSender();
         $room_id = isset($data['room_id']) ? $data['room_id'] : '';
         if (!empty($room_id)) {
-            $flag = $redis::exists($key_client);
-            if (!$flag) {
-                $redis::set($key_client, $room_id);
-            }
-            $redis::set($key_client, $room_id);
+//            $flag = $redis::exists($key_client);
+//            if (!$flag) {
+//                $redis::set($key_client, $room_id);
+//            }
+//
+//            if ($data['type'] == 'img'){
+//                dump(time());
+//            }
+//            $redis::set($key_client, $room_id);
             Room::add(Websocket::getSender(), $room_id);
             Websocket::broadcast()->to([$room_id])->emit('message', $data);
         }
