e74bd1213b05ead0293b3da3266e2ff019ec5969
卢泽明 <luzeming@medlive.cn>
2020-07-10 10:38:08 +0800

    Update p1.blade.php

--- resources/views/muti/video/p1.blade.php
+++ resources/views/muti/video/p1.blade.php
@@ -8,57 +8,63 @@
     <script src="{{asset('assets/nes')}}/m_p1.js"></script>
 </head>
 <body>
-{{--<div style="margin: auto; width: 75%;">--}}
-{{--    <canvas id="nes-canvas" width="256" height="240" style="width: 75%"/>--}}
-{{--</div>--}}
-
-<video style="width: 200px" controls autoplay id="video_div" muted playsinline>
-    <source src="{{ asset('assets') }}/1.mp4" type="video/mp4">
-    您的浏览器不支持 video 标签。
+<div style="margin: auto; width: 75%;">
+    <canvas id="nes-canvas" width="256" height="240" style="width: 75%"/>
+</div>
+<canvas id="nes-canvas2" width="256" height="240" style="width: 75%"/>
+{{--<video style="width: 200px"  autoplay id="video_div" muted playsinline>--}}
+{{--    <source src="{{ asset('assets') }}/1.mp4" type="video/mp4">--}}
+{{--    您的浏览器不支持 video 标签。--}}
+{{--</video>--}}
+{{--<video style="width: 200px"   id="video_div2" autoplay muted playsinline>--}}
 </video>
-<video style="width: 200px" controls  id="video_div2" autoplay muted playsinline>
-</video>
+<img src="" alt="" id="nesimg">
 
 {{--<video id="video_div" controls="controls" src="{{ asset('assets') }}/1.mp4" style="width: 200px" autoplay></video>--}}
-{{--<video id="video_div" playsinline="" class="nes-screen" width="256" height="240"--}}
-{{--       style="width: 100%; position: absolute; image-rendering: pixelated; image-rendering: optimizespeed;"--}}
-{{--       autoplay="" muted=""></video>--}}
-{{--<script>--}}
-{{--    var video = document.getElementById('video_div');--}}
-{{--    var nes_canvas = document.getElementById('nes-canvas');--}}
-{{--    nes_load_url("nes-canvas", "{{ $nes->game }}");--}}
-{{--    let stream = nes_canvas.captureStream();--}}
-
-{{--    console.log(stream);--}}
-{{--    try {--}}
-{{--        console.log('srcObject');--}}
-
-{{--        new_stream = new MediaStream(stream, {--}}
-{{--            mimeType: "video/webm; codecs=h264"--}}
-{{--        });--}}
-{{--        video.srcObject = new_stream;--}}
-{{--    } catch (err) {--}}
-{{--        alert(err);--}}
-{{--    }--}}
-{{--    try {--}}
-{{--        video.play();--}}
-{{--    } catch (err) {--}}
-{{--        alert(err);--}}
-{{--    }--}}
-{{--    document.getElementById("video_div").addEventListener("click", function () {--}}
-{{--        alert(111);--}}
-{{--        video.play();--}}
-{{--    });--}}
-{{--</script>--}}
+<video id="video_div" playsinline="" class="nes-screen" width="256" height="240"
+       style="width: 100%; position: absolute; image-rendering: pixelated; image-rendering: optimizespeed;"
+       autoplay="" muted=""></video>
 <script>
-    var Video1 = document.getElementById('video_div');
-    var video2 = document.getElementById('video_div2');
-    var stream = Video1.captureStream();
-    video2.srcObject = stream;
-    video2.play();
-    // Video1.addEventListener('click', function () {
-    //     video2.srcObject = stream;
-    // });
+    var video = document.getElementById('video_div');
+    var nes_canvas = document.getElementById('nes-canvas');
+    nes_load_url("nes-canvas", "{{ $nes->game }}");
+    let stream = nes_canvas.captureStream();
+
+    console.log(stream);
+    try {
+        console.log('srcObject');
+
+        new_stream = new MediaStream(stream, {
+            mimeType: "video/webm; codecs=h264"
+        });
+        video.srcObject = new_stream;
+    } catch (err) {
+        alert(err);
+    }
+    try {
+        video.play();
+    } catch (err) {
+        alert(err);
+    }
+    document.getElementById("video_div").addEventListener("click", function () {
+        alert(111);
+        video.play();
+    });
+    const canvas = window.canvas = document.getElementById('nes-canvas2');
+
+
+    // var Video1 = document.getElementById('video_div');
+    // var video2 = document.getElementById('video_div2');
+    // var stream = Video1.captureStream();
+    // video2.srcObject = stream;
+    // video2.play();
+    makeImg();
+    function makeImg() {
+        setInterval(setImg, 150);
+    }
+    function setImg() {
+        canvas.getContext('2d').drawImage(video, 0, 0, 256, 240);
+    }
 </script>
 </body>
 </html>
